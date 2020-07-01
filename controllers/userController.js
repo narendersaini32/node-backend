584c29aaab72b4e2901eced86800c936f82a431b
Naruto <naruto@ryaz.io>
2020-07-01 15:25:33 +0530

    bcrypt pass enecypted added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,15 @@
+const bcrypt = require('bcrypt');
+
 const { User } = require("../model/userSchema");
 
 const registerController = async (req, res) => {
     try {
         const { data } = req.body;
-        const { email } = data;
+        const { email,password } = data;
+        const encPass = await bcrypt.hash(password, 10);
         const userList = await User.find({ email });
         if (!userList.length) {
-            const result = await User.create(data);
+            const result = await User.create({email,password:encPass});
             res.send({
                 success: true,
                 message: "User Addeed successfully",
